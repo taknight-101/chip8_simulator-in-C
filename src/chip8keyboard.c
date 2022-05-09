e8b172635bb11e3ce63c4b1ad922918f04c785d4
Ahmed Ibrahim <taknight101.work@protonmail.com>
2022-05-09 07:16:34 +0200

    Implemented chip8_init()

--- src/chip8keyboard.c
+++ src/chip8keyboard.c
@@ -5,8 +5,6 @@ static void chip8_keyboard_ensure_in_bounds(int key)
     assert(key >= 0 && key < CHIP8_TOTAL_KEYS);
 }
 
-
-// map physical key to vkey "virtual key" ; vkey == hex(index) essentially in chip8 
 int chip8_keyboard_map(const char* map, char key)
 {
     
@@ -21,17 +19,16 @@ int chip8_keyboard_map(const char* map, char key)
     return -1;
 }
 
-// key == vkey 
 void chip8_keyboard_down(struct chip8_keyboard* keyboard, int key)
 {
     keyboard->keyboard[key] = true;
 }
-// key == vkey 
+
 void chip8_keyboard_up(struct chip8_keyboard* keyboard, int key)
 {
     keyboard->keyboard[key] = false;
 }
-// key == vkey 
+
 bool chip8_keyboard_is_down(struct chip8_keyboard* keyboard, int key)
 {
     return keyboard->keyboard[key];
